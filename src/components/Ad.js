3a13a9826a178935b370b0367ac3c6787dd597c7
Qujuan Miller <qujuan.miller@snhu.edu>
2023-05-27 23:28:16 -0400

    ads added

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -10,8 +10,8 @@ render () {
       <div className='ad'>
         <ins className='adsbygoogle'
           style={{ display: 'block' }}
-          data-ad-client='ca-pub-xxxxxxxxxx'
-          data-ad-slot='xxxxxxxxxx'
+          data-ad-client='ca-pub-8397585981018540'
+          data-ad-slot='6609253060'
           data-ad-format='auto' />
       </div>
     );
