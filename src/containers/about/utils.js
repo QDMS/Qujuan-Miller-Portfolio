b1d3bcacac415b20ac967d5df5792c64c0e34fea
Qujuan <qujuan.miller@snhu.edu>
2023-08-17 06:59:23 -0400

    Changed Phone Nmber

--- src/containers/about/utils.js
+++ src/containers/about/utils.js
@@ -1,26 +1,26 @@
 export const personalData = [
-    {
-        label: 'Name: ',
-        value: 'Qujuan Miller'
-    },
-    {
-        label: 'Age: ',
-        value: '37'
-    },
-    {
-        label: 'Address: ',
-        value: 'Valdosta, Ga'
-    },
-    {
-        label: 'Email: ',
-        value: 'qujuanmiller@gmail.com'
-    },
-    {
-        label: 'Phone Number: ',
-        value: '229-929-0126'
-    },
-    {
-        label: 'Military Status: ',
-        value: 'Navy Veteran'
-    },
-]
+  {
+    label: "Name: ",
+    value: "Qujuan Miller",
+  },
+  {
+    label: "Age: ",
+    value: "37",
+  },
+  {
+    label: "Address: ",
+    value: "Valdosta, Ga",
+  },
+  {
+    label: "Email: ",
+    value: "qujuan.miller@snhu.edu",
+  },
+  {
+    label: "Phone Number: ",
+    value: "229-757-1502",
+  },
+  {
+    label: "Military Status: ",
+    value: "Navy Veteran",
+  },
+];
